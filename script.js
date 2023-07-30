28cca13ea76ee352702d37e9a0642f530a330f7f
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-07-29 17:04:27 -0700

    landing page still in progress

---
+++ script.js
@@ -0,0 +1,31 @@
+const textHolder = document.getElementById('textHolder');
+const paragraphs = [
+    { text: "Do you have a dream? We will share it with you and make it real", color: "blue"},
+    { text: "Do you have a plan? We will develop it untill it becomes alive", color: "orange"},
+    { text: "Do you desire a life? We will breathe into it to make it true", color: "grey"},
+];
+let currentParagraphIndex = 0;
+
+function showNextParagraph() {
+    const currentParagraph = paragraphs[currentParagraphIndex];
+    textHolder.textContent = currentParagraph.text;
+    textHolder.style.color = currentParagraph.color;
+    textHolder.style.opacity = 1;
+
+    // Wait for a certain duration before hiding the current paragraph
+    setTimeout(function () {
+        textHolder.style.opacity = 0;
+
+        // Move to the next paragraph
+        currentParagraphIndex++;
+
+        if (currentParagraphIndex === paragraphs.length) {
+            currentParagraphIndex = 0;
+        }
+
+        // Show the next paragraph after a brief delay
+        setTimeout(showNextParagraph, 1000); 
+    }, 3500); 
+}
+
+showNextParagraph();
