087f2f159f84d8aadaf6bbd544247316467fc5d6
Its-me-ravi <rp808462@gmail.com>
2025-04-26 10:58:29 +0530

    image

--- script.js
+++ script.js
@@ -281,3 +281,16 @@ ScrollReveal().reveal((footcc), {
   easing: 'ease-in',
   viewFactor: 0.3
 });
+
+// extra
+
+const images = document.querySelectorAll('.hero-img img');
+    let currentIndex = 0;
+
+    function showNextImage() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('active');
+    }
+
+    setInterval(showNextImage, 2000);
